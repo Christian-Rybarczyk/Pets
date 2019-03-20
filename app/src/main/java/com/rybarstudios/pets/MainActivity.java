f7c139945ee557553382bd98371c3006636ed9dc
Christian Rybarczyk <christian.j.rybarczyk@gmail.com>
2019-03-20 15:40:24 -0400

    Created generate TextView method

--- app/src/main/java/com/rybarstudios/pets/MainActivity.java
+++ app/src/main/java/com/rybarstudios/pets/MainActivity.java
@@ -1,13 +1,26 @@
 package com.rybarstudios.pets;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = this;
+    }
+
+    private TextView generateTextView(String animal) {
+        TextView view = new TextView(context);
+        view.setTextSize(TypedValue.COMPLEX_UNIT_SP, 24);
+        view.setText(animal);
+        return view;
     }
 }
