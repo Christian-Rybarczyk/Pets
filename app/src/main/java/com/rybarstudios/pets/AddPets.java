96501cbc06334a639c11ed59cee88b67037a5018
Christian Rybarczyk <christian.j.rybarczyk@gmail.com>
2019-03-20 15:59:12 -0400

    Added listener to button along with an intent to send animal names to animal list

--- app/src/main/java/com/rybarstudios/pets/AddPets.java
+++ app/src/main/java/com/rybarstudios/pets/AddPets.java
@@ -1,12 +1,16 @@
 package com.rybarstudios.pets;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class AddPets extends AppCompatActivity {
 
+    Context context;
     EditText mEditText;
     Button mButton;
 
@@ -14,9 +18,19 @@ public class AddPets extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_pets);
+        context = this;
 
         mEditText = findViewById(R.id.edit_pet_name_entry);
         mButton = findViewById(R.id.button_add_pet);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String animalName = mEditText.getText().toString();
+                Intent intent = new Intent(context, MainActivity.class);
+                intent.putExtra(Intent.EXTRA_TEXT, animalName);
+            }
+        });
 
     }
 }
