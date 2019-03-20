cc4eb392c74e2bc9c3f84c5f6fd8cddfc7db304f
Christian Rybarczyk <christian.j.rybarczyk@gmail.com>
2019-03-20 17:26:25 -0400

    Redid app to just a single activity

--- app/src/main/java/com/rybarstudios/pets/MainActivity.java
+++ app/src/main/java/com/rybarstudios/pets/MainActivity.java
@@ -5,12 +5,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.TypedValue;
+import android.view.Gravity;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Context context;
+    String[] animalName = {"Husky", "German Shepard", "Tiger", "Penguin", "Bear", "Wolf", "Panda"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,16 +20,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         context = this;
 
-        Intent intent = getIntent();
-        String animalName = intent.getStringExtra(Intent.EXTRA_TEXT);
-
-        ((LinearLayout)findViewById(R.id.view_animal_list)).addView(generateTextView(animalName));
+        for(String ele : animalName) {
+            ((LinearLayout)findViewById(R.id.view_animal_list)).addView(generateTextView(ele));
+        }
     }
 
     private TextView generateTextView(String animal) {
         TextView view = new TextView(context);
         view.setTextSize(TypedValue.COMPLEX_UNIT_SP, 24);
         view.setText(animal);
+        view.setGravity(Gravity.CENTER);
+        view.setTextColor(getResources().getColor(R.color.emerald));
         return view;
     }
 }
