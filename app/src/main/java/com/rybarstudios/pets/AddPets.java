14aba38f445c03d986782b1e38c08e67371670b5
Christian Rybarczyk <christian.j.rybarczyk@gmail.com>
2019-03-20 15:55:44 -0400

    Added handlers to EditText and Button views

--- app/src/main/java/com/rybarstudios/pets/AddPets.java
+++ app/src/main/java/com/rybarstudios/pets/AddPets.java
@@ -2,12 +2,21 @@ package com.rybarstudios.pets;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AddPets extends AppCompatActivity {
+
+    EditText mEditText;
+    Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_pets);
+
+        mEditText = findViewById(R.id.edit_pet_name_entry);
+        mButton = findViewById(R.id.button_add_pet);
+
     }
 }
